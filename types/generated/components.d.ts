08b8344a67e31e3b26b98ae33ab4fab4eaec8c30
Emmanuel Olivo <emmanuel@wealthcharts.com>
2024-05-29 12:02:13 -0500

    Add Home page

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,35 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ComponentsLink extends Schema.Component {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    url: Attribute.String;
+    label: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    description: '';
+  };
+  attributes: {
+    heading: Attribute.Text;
+    primaryLink: Attribute.Component<'components.link'>;
+    secondaryLink: Attribute.Component<'components.link'>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'components.link': ComponentsLink;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
 }
